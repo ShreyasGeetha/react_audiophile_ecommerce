9163e665591e40600983e4ce4560980b0a423a88
ShreyasGeetha <shreyasgeetha@gmail.com>
2022-05-03 22:26:28 +0530

    'products'

--- src/components/Product/Breadcrum.js
+++ src/components/Product/Breadcrum.js
@@ -2,6 +2,7 @@ import React from 'react'
 
 const Breadcrum = () => {
   return (
+    // takes your back to the homepage. In future, each product page link can be added as well
     <div className='product__breadcrum body__text'>
       GoBack
     </div>
